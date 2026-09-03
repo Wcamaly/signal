924ef02001c61855329de4930fd98f8b8b6d123a
Walter Camaly <wcamaly@gocloud.la>
2026-09-03 15:03:31 +0000

    Fix local image transfer path

--- deploy/load-local-image-k3s.sh
+++ deploy/load-local-image-k3s.sh
@@ -26,13 +26,13 @@ trap cleanup EXIT
 "$KUBECTL" -n "$NAMESPACE" run "$POD" \
   --image=alpine:3.22 \
   --restart=Never \
-  --overrides='{"spec":{"nodeName":"k3s-master","hostPID":true,"hostNetwork":true,"containers":[{"name":"loader","image":"alpine:3.22","command":["sleep","3600"],"securityContext":{"privileged":true},"volumeMounts":[{"name":"host-root","mountPath":"/host"},{"name":"image-transfer","mountPath":"/transfer"}]}],"volumes":[{"name":"host-root","hostPath":{"path":"/"}},{"name":"image-transfer","hostPath":{"path":"/tmp/signal-image-loader","type":"DirectoryOrCreate"}}]}}' \
+  --overrides='{"spec":{"nodeName":"k3s-master","hostPID":true,"hostNetwork":true,"containers":[{"name":"loader","image":"alpine:3.22","command":["sleep","3600"],"securityContext":{"privileged":true},"volumeMounts":[{"name":"host-root","mountPath":"/host"}]}],"volumes":[{"name":"host-root","hostPath":{"path":"/"}}]}}' \
   --image-pull-policy=IfNotPresent >/dev/null
 
 "$KUBECTL" -n "$NAMESPACE" wait --for=condition=Ready pod/$POD --timeout=120s >/dev/null
-"$KUBECTL" -n "$NAMESPACE" cp "$TAR_FILE" "$POD:/transfer/signal-image.tar"
+"$KUBECTL" -n "$NAMESPACE" cp "$TAR_FILE" "$POD:/host/tmp/signal-image.tar"
 "$KUBECTL" -n "$NAMESPACE" exec "$POD" -- /bin/sh -c \
-  'chroot /host /usr/local/bin/k3s ctr -n k8s.io images import /host/tmp/signal-image-loader/signal-image.tar'
+  'chroot /host /usr/local/bin/k3s ctr -n k8s.io images import /tmp/signal-image.tar'
 
 echo "Imagen $IMAGE importada en k3s. Reiniciando Signal…"
 "$KUBECTL" -n "$NAMESPACE" rollout restart deployment/signal
